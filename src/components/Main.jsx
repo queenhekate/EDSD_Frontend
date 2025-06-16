c560f9f10b7d5e74c96487384328e8c94f032956
Johanna Schnell <johannaschnell@gmail.com>
2025-06-16 13:55:40 -0700

    debug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const SHEET_URL =
-  "https://docs.google.com/spreadsheets/d/18q40KGtN2lLllszVcjh2tamkksFURcZTR5w5xM1qbZ0/export?format=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZJ6FSAFTBSM-XiLRNgtuIJ6vBJKPqMotM7r3JZo8OYctPfaI1kwZ-5FdQZoZOkpH1O7TZQgkq9uqa/pub?output=csv";
 
 function parseCSV(text) {
   const rows = text.trim().split("\n");
